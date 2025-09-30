c150e0a9a8ed743d1224e276a5915b5b659d6d8f
Guaaannnn <mfatkhanptr@gmail.com>
2025-09-30 19:19:54 +0700

    fix: bug at jumping feature

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,27 +4,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
+    private int jumpCount;
+    [SerializeField] private int maxJumps = 2;
 
     float horizontalMovement;
     private bool isGrounded;
-   
-    
-
-    public Transform groundCheck;
-    public float checkRadius = 0.2f;
-    public LayerMask groundLayer;
-
-    void Start()
-    {
-
-    }
 
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -34,10 +25,30 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        if (context.performed && jumpCount < maxJumps)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpCount++;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpCount = 0;
+            Debug.Log("Player di TANAH");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            Debug.Log("Player di UDARA");
         }
     }
 }
-
